cde1736200639e500722b3e8c857b5edbe58de9d
DucNguyen <ducnguyenvh9009@gmail.com>
2025-05-31 09:29:28 +0700

    update

--- frontend/src/pages/Auth/Signin/ForgotPassword.js
+++ frontend/src/pages/Auth/Signin/ForgotPassword.js
@@ -19,7 +19,7 @@ function ForgotPassword() {
     e.preventDefault();
 
     if (!email.trim()) {
-      alert("Vui lòng nhập email của bạn!");
+      alert("Vui lòng nhập email!");
       return;
     }
 
